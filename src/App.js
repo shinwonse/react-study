b31fd36159ba2829e77933278a10236e607a0f1d
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2021-11-25 15:22:02 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useState } from "react";
+import { Route } from "react-router-dom";
+import Categories from "./components/Categories";
+import NewsList from "./components/NewsList";
+import NewsPage from "./pages/NewsPage";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  return <Route path="/:category?" component={NewsPage} />;
+};
 
 export default App;
